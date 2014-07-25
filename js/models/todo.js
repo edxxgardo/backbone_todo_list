a4ac71936d0af5eec388cccc351d6bdd2ca04bb9
heldrida <heldrida@gmail.com>
2014-07-25 16:35:05 +0000

    created initial structured. model and collection js files. created assets, css and image file.

---
+++ js/models/todo.js
@@ -0,0 +1,19 @@
+var app = app || {};
+
+// Todo Model
+
+app.Todo = Backbone.Model.extend({
+
+	// Default attributes ensure that each todo created has title and completed keys
+	defaults: {
+		title: '',
+		completed: false
+	},
+
+	toggle: function(){
+		this.save({
+			completed: !this.get('completed')
+		});
+	}
+
+});
